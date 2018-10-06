d76d795e3fefc7a92b5488a5db6151c5b44b35fd
Mark Meijerman <mark@future500.nl>
2018-10-06 20:30:42 +0200

    new text

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -162,8 +162,9 @@ OET;
             case 'whatItCosts':
                 $content = <<<OET
 <p>De Herstelkamer is een particulier bedrijf en ingeschreven bij de Kamer van Koophandel. U bent zelf verantwoordelijk voor betaling.</p> 
-<p>Een verblijf in de Herstelkamer kost € 145,- per nacht inclusief BTW. Hierbij zijn 3 maaltijden, koffie- en thee inbegrepen. 
+<p>Een verblijf in Herstelkamer Nootdorp kost € 165,- per nacht inclusief BTW. Hierbij zijn 3 maaltijden, koffie- en thee inbegrepen. 
 Gebruik van de minibar en het op uw verzoek draaien van een was wordt apart in rekening gebracht.</p>
+<p><b>Tot 1 juli 2019 geldt een speciaal actietarief van € 145,- per nacht inclusief BTW.</b></p>
 <p>Bij reservering wordt een aanbetaling van 50% gevraagd. Het eindbedrag dient op de dag van vertrek te worden voldaan.</p>
 <p>Voor niet-inwoners van de gemeente Pijnacker-Nootdorp ben ik verplicht toeristenbelasting in rekening te brengen. Deze bedraagt € 2,12 per nacht.</p>
 OET;
