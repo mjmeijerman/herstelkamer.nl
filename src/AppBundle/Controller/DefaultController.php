c0132a31ce525f63860017fcb2a6ae790ab73f34
Mark Meijerman <mark@future500.nl>
2021-05-03 09:25:28 +0200

    update text

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -248,7 +248,7 @@ OET;
             case 'whatItCosts':
                 $content = <<<OET
 <p>De Herstelkamer is een particulier bedrijf en ingeschreven bij de Kamer van Koophandel. U bent zelf verantwoordelijk voor betaling.</p> 
-<p>Tot 31 juli 2019 betaalt u het voordelige introductietarief van € 95,- per nacht. Hierbij zijn 3 maaltijden, koffie- en thee inbegrepen. Bij reservering wordt een aanbetaling van 50% gevraagd. Het eindbedrag dient op de dag van vertrek te worden voldaan.</p>
+<p>Een overnachting kost €135,- per nacht. Hierbij inbegrepen is €10,- voor het extra Corona-schoonmaakprotocol. Hierbij zijn 3 maaltijden, koffie- en thee inbegrepen. Bij reservering wordt een aanbetaling van 50% gevraagd. Het eindbedrag dient op de dag van vertrek te worden voldaan.</p>
 <p>Voor niet-inwoners van de gemeente Pijnacker-Nootdorp ben ik verplicht toeristenbelasting in rekening te brengen. Deze bedraagt € 2,12 per nacht.</p>
 OET;
 
